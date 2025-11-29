62248686f3df6aaa75b52c8e2adc0e06f6d93675
bubuns <tornadowolverine@gmail.com>
2025-11-29 15:21:39 +0300

    Add config: добавлен файл конфигурации

---
+++ config.py
@@ -0,0 +1,13 @@
+"""
+Конфигурационный файл приложения
+"""
+
+# Настройки приложения
+APP_NAME = "Exam Application"
+APP_VERSION = "1.0.0"
+DEBUG = False
+
+# Настройки сервера
+HOST = "localhost"
+PORT = 8080
+
